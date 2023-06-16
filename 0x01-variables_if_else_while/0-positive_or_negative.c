8418b4ceb8a09659be2a24bd386be460579797a6
SiSimo9750 <bourhim.mo8@gmail.com>
2023-06-16 15:39:09 -0700

    Betty documentation

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
+/**
  * Main - To show a random generated number n
  *     and show if it is negative or positive
  *     or equal to 0

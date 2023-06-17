2b614f7c359c7a4ae90fcf5010ac6abee675b2f0
SiSimo9750 <bourhim.mo8@gmail.com>
2023-06-16 19:00:42 -0700

    Task 8 and 9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - will display base 16 numbers
+ * return: will return 0
+ */
+int main(void)
+{
+	int a;
+	char b;
+
+	for (a = 48; a < 58; a++)
+	{
+	putchar(a);
+	}
+	for (b = 'a'; b < 'g'; b++)
+	{
+	putchar(b);	
+	}
+	putchar('\n');
+	return (0);
+}

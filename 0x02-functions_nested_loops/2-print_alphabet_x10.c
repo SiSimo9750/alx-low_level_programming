3d6e3438657166056b45d64bf1b94b83713d1b34
SiSimo9750 <bourhim.mo8@gmail.com>
2023-06-20 17:01:55 -0700

    Task 1 and 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - will print alphabets x 10
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int New_line = 0;
+	char alp = 'a';
+
+	while (New_line < 10)
+	{
+		while (alp <= 'z')
+			{
+				_putchar('alp');
+				alp++;
+			}
+	}
+	_putchar('\n');
+	New_line++;
+}

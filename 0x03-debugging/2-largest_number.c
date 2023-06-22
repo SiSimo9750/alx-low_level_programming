41d8c7839e6362af26da37febaa85897575e5640
SiSimo9750 <bourhim.mo8@gmail.com>
2023-06-22 16:26:49 -0700

    add printf function

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,19 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if ((a > b) && (a > c))
 	{
 	largest = a;
 	}
-	else if (b > a && b > c)
+	else if ((b > a) && (b > c))
 	{
 	largest = b;
 	}
-	else if (c > a && c > b)
+	else if ((c > a) && (c > b))
 	{
 	largest = c;
 	}
+	printf("%d is the largest number", largest);
 
 	return (largest);
 }

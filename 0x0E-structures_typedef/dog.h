552dd59ad317e79d8a0731486f4e438518af30d5
SiSimo9750 <bourhim.mo8@gmail.com>
2023-07-24 13:34:24 -0700

    committing the header for the first task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_HEADER
+#define DOG_HEADER
+/**
+ * struct dog - dog object which contains the dog's infos
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the owner name
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

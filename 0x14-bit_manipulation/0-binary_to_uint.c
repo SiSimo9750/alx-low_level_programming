d68b7462ea813731909fcb46fcf2ff2bc50fce4b
SiSimo9750 <bourhim.mo8@gmail.com>
2023-08-31 17:11:23 -0700

    header file and task 0 code

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts binary to unsigned int
+ * @b: Input binary string
+ *
+ * Return: The converted unsigned int or 0 if input is not binary
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	unsigned int power_of_two = 1;
+	int length = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[length] != '\0')
+	{
+	if (b[length] != '0' && b[length] != '1')
+		return (0);
+		length++;
+	}
+
+	for (length--; length >= 0; length--, power_of_two *= 2)
+	{
+		if (b[length] == '1')
+			result += power_of_two;
+	}
+
+	return (result);
+}

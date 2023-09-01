9d7ccb252ab84c879353e74789372739e4aa0657
SiSimo9750 <bourhim.mo8@gmail.com>
2023-09-01 08:14:34 -0700

    Pushing advanced tasks

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - will verify endianness
+ * Return: 0 or 1 depends on endianness
+ */
+int get_endianness(void)
+{
+	int i = 1;
+
+	if (((i >> 7) & 1) == 1)
+		return (1);
+	else
+		return (0);
+}
